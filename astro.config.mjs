a3817748c635f4706997a26d232c81b49c0c0e1a
brave-dev-git <jonas@bravesecurity.se>
2026-02-13 11:55:49 +0100

    minor adjustment in text and update of always / in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bravesecurity.se",
+  trailingSlash: "always", //Inlagd pga SEO, så att alla URL:er slutar med "/". Annars kan det bli problem med sökmotorer som inte hanterar URL:er utan trailing slash korrekt.
   integrations: [sitemap()],
 });
